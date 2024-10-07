3127d2323874dfdf500a635342d0305dd19ed1bc
sneta gupta <snetagupta4nov@gmail.com>
2024-10-07 16:57:22 +0530

    fix bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import AppContextProvider from './context/AppContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <AppContextProvider>
-    <App />
-    </AppContextProvider>
-  </BrowserRouter>,
-)
+    <StrictMode>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </StrictMode>
+  </BrowserRouter>
+);
